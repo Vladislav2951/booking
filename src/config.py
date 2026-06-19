bb4ab4d19df28c5057141fdb2dd31562d825aab9
Vladislav <vladislav.dr2@mail.ru>
2026-06-19 03:17:33 +0300

    chore: edit env

--- src/config.py
+++ src/config.py
@@ -1,7 +1,9 @@
+from ipaddress import IPv4Address
 from pathlib import Path
 import tomllib
-from typing import Literal
+from typing import Literal, Union
 
+from pydantic import IPvAnyAddress, computed_field
 from pydantic_settings import BaseSettings
 
 
@@ -19,13 +21,30 @@ def _get_version_from_pyproject():
 
 
 class Settings(BaseSettings):
-    DATABASE_URI: str = "postgresql+asyncpg://postgres:password@localhost:5432/booking_db"
+    POSTGRES_HOST: str = "postgres"
+    POSTGRES_PORT: int = 5432
+    POSTGRES_USER: str = "postgres"
+    POSTGRES_PASSWORD: str = "password"
+    POSTGRES_DB: str = "booking_db"
+
     REDIS_URI: str = "redis://localhost:6379/0"
     RATE_LIMIT_STORAGE_URI: str = "memory://"
-    APP_ENV: Literal["dev", "prod", "test"] = "dev"
     VERSION: str = _get_version_from_pyproject()
 
     model_config = {"env_file": ".env", "extra": "ignore"}
+
+    @computed_field  # type: ignore[prop-decorator]
+    @property
+    def DATABASE_URI(self) -> str:
+        return (
+            "postgresql+asyncpg://{username}:{password}@{host}:{port}/{database}".format(
+                username=self.POSTGRES_USER,
+                password=self.POSTGRES_PASSWORD,
+                host=self.POSTGRES_HOST,
+                port=self.POSTGRES_PORT,
+                database=self.POSTGRES_DB,
+            )
+        )
 
 
 settings = Settings()
